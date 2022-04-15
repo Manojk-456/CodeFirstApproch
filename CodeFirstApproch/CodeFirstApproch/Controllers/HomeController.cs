40a52e1f29a8edd9513833ae18a901d7bd4533e4
unknown <admin@CHETUIWK217.chetu.com>
2022-04-15 16:03:59 +0530

    second

---
+++ CodeFirstApproch/CodeFirstApproch/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using CodeFirstApproch.Db_Context;
+using CodeFirstApproch.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirstApproch.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDataContext _db;
+        public HomeController(AppDataContext db)
+        {
+            _db = db;
+        }
+
+       
+
+        public IActionResult Index()
+        {
+            var res = _db.students.ToList();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CodeFirstApproch/CodeFirstApproch/Db_Context/AppDataContext.cs
@@ -0,0 +1,18 @@
+﻿using CodeFirstApproch.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirstApproch.Db_Context
+{
+    public class AppDataContext :DbContext
+    {
+        public AppDataContext(DbContextOptions<AppDataContext> options): base(options)
+        {
+
+        }
+        public DbSet<StudentModel> students { get; set; }
+    }
+}
